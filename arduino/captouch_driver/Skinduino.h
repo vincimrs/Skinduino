7482bdbc80cebe8f2fd927ca882f762287601534
Andres <andresc@mit.edu>
2017-01-16 00:57:03 -0500

    Separate Skinduino.cpp file from .h file

--- arduino/captouch_driver/Skinduino.h
+++ arduino/captouch_driver/Skinduino.h
@@ -1,6 +1,7 @@
 #ifndef SKINDUINO_h
 #define SKINDUINO_h
 
+#include "Arduino.h"
 #include <Wire.h>
 
 class Skinduino {
@@ -9,37 +10,13 @@ class Skinduino {
     
     Skinduino() {}
     
-    void setCapacitiveBaseline() { 
-      Wire.beginTransmission(i2caddr_default);
-      Wire.write(0x04);
-      Wire.write(0x01);
-      Wire.endTransmission();
-    }
-
-    void readSensorValues() {
-      resetSensorValues();
-      
-      counter = 0;
-      Wire.beginTransmission(i2caddr_default); 
-      Wire.write(0x81); ///only the 1-8 are broken out
-      Wire.endTransmission(false);
-      Wire.requestFrom(0x25,8);  
-      while(Wire.available()) { 
-        sensorValues[7-counter]= Wire.read();
-        counter++;
-      }
-    }
+    void setCapacitiveBaseline();
+    void readSensorValues();
     
   private:
-    int counter = 0;
-    uint8_t i2caddr_default = 0x25;
-
-
-    void resetSensorValues() { 
-      for(int i=0; i<15; i++){
-        sensorValues[i] = 0;
-      }  
-    }
+    int _counter;
+    uint8_t _i2caddr_default = 0x25;
+    void resetSensorValues();
 };
 
 #endif
